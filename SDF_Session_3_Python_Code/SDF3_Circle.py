37bb7347cbb0e4aaf47738f1f5496fac5297f22a
standford1 <standfordhlatshwayo54@gmail.com>
2024-09-11 20:14:41 +0200

    Add files via upload

---
+++ SDF_Session_3_Python_Code/SDF3_Circle.py
@@ -0,0 +1,8 @@
+#Write a program to find area of circle
+# Area of circle= PI *radius*radius
+#Step 1: Read radius of circle
+radius=float(input("Enter radius of circle:"))
+#step 2: Calculate area of circle
+area= 3.14*radius*radius
+#Step3 : Display area of circle
+print("Area of circle is:",area)
---
+++ SDF_Session_3_Python_Code/SDF_p1 (3).py
@@ -0,0 +1,6 @@
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+#num1=int(num1)
+#num2=int(num2)
+sum=num1+num2
+print("The addition of",num1,"and",num2,"is",sum)
